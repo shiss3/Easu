f3f7a85f7fc574e7942f5922aed460d77d40262c
shiss3 <shiss2002@outlook.com>
2026-02-26 15:05:30 +0800

    feat(hotel): 为查询页增加底部导航栏

--- apps/client-user/src/App.tsx
+++ apps/client-user/src/App.tsx
@@ -4,6 +4,7 @@ import SearchResultPage from './page/search-result';
 import HotelDetailPage from "@/page/hotel-detail";
 import LoginPage from "@/page/login";
 import AIAssistantPage from "@/page/ai-assistant";
+import MobileLayout from "@/layouts/MobileLayout";
 import { Toaster } from 'sonner';
 
 function App() {
@@ -11,17 +12,13 @@ function App() {
         <BrowserRouter>
             <Toaster position="top-center" richColors />
             <Routes>
-                <Route path="/">
+                <Route element={<MobileLayout />}>
                     <Route index element={<HomePage />} />
-                    {/* 搜索结果页 */}
-                    <Route path="search" element={<SearchResultPage />} />
-                    {/*酒店详情页*/}
                 </Route>
+                <Route path="/search" element={<SearchResultPage />} />
                 <Route path="/hotel/:id" element={<HotelDetailPage />} />
-                {/* AI 助手页 */}
                 <Route path="/ai-assistant" element={<AIAssistantPage />} />
-                {/*登陆页*/}
-                    <Route path="/login" element={<LoginPage />} />
+                <Route path="/login" element={<LoginPage />} />
             </Routes>
         </BrowserRouter>
     );
